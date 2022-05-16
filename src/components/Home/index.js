8fa0633eb8cb4de98c91ec36b5e00fd9442d476a
minha619 <minha619@gmail.com>
2022-05-16 18:27:02 -0400

    checkout button  working

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
+
 
 function Home() {
+    let history = useHistory();
+    const onclick = () => {
+        // window.location.replace('/Projects')
+        history.push('/Projects')
+    }
     return (
         <div class='main-container'>
             <div id='home-container'>
                 Hello, <br></br>
                 I'm Minha Kim. <br></br>
                 I'm a front-end web developer.
-                <button class="uk-button uk-button-default" uk-tooltip="title: VIEW PORTFOLIO; pos: bottom" >Check out more</button>
+                <button onClick={onclick} class="uk-button uk-button-default" uk-tooltip="title: VIEW PORTFOLIO; pos: bottom" >Check out more</button>
             </div>
         </div>
     );
 }
 
 export default Home;
+
