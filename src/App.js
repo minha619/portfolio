fc39e9017873c3960ed8ad3b025d55765dd60db1
minha619 <minha619@gmail.com>
2022-05-18 22:43:50 -0400

    app.js update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <BrowserRouter>
       <Nav></Nav>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="portfolio/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects currentCategory={currentCategory}> </Projects>} />
         <Route path="/contact" element={<Contact />} />
