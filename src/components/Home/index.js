04e28dc3903887094bc76f8939489ff85fe6d864
minha619 <minha619@gmail.com>
2022-05-18 20:15:04 -0400

    responsive update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { useHistory } from "react-router-dom";
+import {Link} from 'react-router-dom';
+
 
 
 function Home() {
-    let history = useHistory();
-    const onclick = () => {
-        // window.location.replace('/Projects')
-        history.push('/Projects')
-    }
+
     return (
         <div class='main-container'>
             <div id='home-container'>
                 Hello, <br></br>
                 I'm Minha Kim. <br></br>
                 I'm a front-end web developer.
-                <button onClick={onclick} class="uk-button uk-button-default" uk-tooltip="title: VIEW PORTFOLIO; pos: bottom" >Check out more</button>
+                <button class="uk-button uk-button-default" uk-tooltip="title: VIEW PORTFOLIO; pos: bottom" > <Link to="/Projects">Check out more </Link> </button>
             </div>
         </div>
     );
